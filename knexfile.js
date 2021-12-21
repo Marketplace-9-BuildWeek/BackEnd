203bfcf2d989018818e5266269f8726381bb0b3e
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-21 15:41:52 -0600

    idk

--- knexfile.js
+++ knexfile.js
@@ -16,27 +16,27 @@ require('dotenv').config()
     - testing_database_name (use the real name of the testing database you created in pgAdmin 4)
 
 */
-// const pg = require('pg')
-const {Pool} = require('pg')
+const pg = require('pg')
+// const {Pool} = require('pg')
 
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = false
-// }
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = false
+}
 
-const pool = (() => {//eslint-disable-line
-  if (process.env.NODE_ENV !== 'production') {
-      return new Pool({
-          connectionString: process.env.DATABASE_URL,
-          ssl: false
-      });
-  } else {
-      return new Pool({
-          connectionString: process.env.DATABASE_URL,
-          ssl: {
-              rejectUnauthorized: false
-            }
-      });
-  } })();
+// const pool = (() => {//eslint-disable-line
+//   if (process.env.NODE_ENV !== 'production') {
+//       return new Pool({
+//           connectionString: process.env.DATABASE_URL,
+//           ssl: false
+//       });
+//   } else {
+//       return new Pool({
+//           connectionString: process.env.DATABASE_URL,
+//           ssl: {
+//               rejectUnauthorized: false
+//             }
+//       });
+//   } })();
 
 const sharedConfig = {
   client: 'pg',
