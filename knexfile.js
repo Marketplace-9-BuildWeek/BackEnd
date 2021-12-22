539a89c65293e997ecc9454827a423ffb9470b94
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-22 11:05:01 -0600

    hm

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ const pg = require('pg')
 // const {Pool} = require('pg')
 
 if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = false
+  pg.defaults.ssl = {rejectUnauthorized = false}
 }
 
 // const pool = (() => {//eslint-disable-line
